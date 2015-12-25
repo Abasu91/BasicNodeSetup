08ffc3c681637656d1c24ea027c16e48cc73526e
Abasu91 <arijitbasu948@gmail.com>
2015-12-25 12:30:53 -0600

    minor fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 //-- '/' is a route handler that gets called when we hit our website home
 app.get('/', function(req, res){
-  res.send('<h1>Merry Christmas</h1>');
+  res.send('<h1>Merry Christmas and a Happy New Year</h1>');
 });
 //--we make the server listen to port number 3000
 http.listen(3000, function(){
